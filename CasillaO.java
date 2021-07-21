e4c89b029d1457dac7a950b5e2e9aaa536583435
jpc512 <jpcordon512@gmail.com>
2021-07-21 00:01:37 -0600

    Casillas de colores

---
+++ CasillaO.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CasillaO here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CasillaO extends Casilla
+{
+    /**
+     * Act - do whatever the CasillaO wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
---
+++ CasillaY.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CasillaY here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CasillaY extends Casilla
+{
+    /**
+     * Act - do whatever the CasillaY wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
